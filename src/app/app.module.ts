7b3416fe540bfcb308a1903677adab933f792d4a
rhernandezgil <raul.hergil@educa.jcyl.es>
2020-04-07 13:11:14 +0200

    Header Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SocialBarComponent } from './common/social-bar/social-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SocialBarComponent
   ],
   imports: [
     BrowserModule,
